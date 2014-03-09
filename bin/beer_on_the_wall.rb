774c32daf1d51634275d81556f0939ca2dc55a8c
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-03-09 13:11:38 -0600

    Removed 3rd pending

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -1,6 +1,15 @@
 puts "How many bottles of beer?"
 bottles = Integer(gets.chomp)
-while bottles > 0
-  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\nTake one down and pass it around, #{bottles -= 1} bottles of beer on the wall.\n"
+while bottles >= 0
+  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\nTake one down and pass it around, #{bottles - 1} bottles of beer on the wall.\n"
+  if bottles == 2
+    puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\nTake one down and pass it around, #{bottles - 1} bottle of beer on the wall.\n"
+  end
+  bottles -= 1
 end
 
+# bottles == 1
+
+# bottles == 0
+
+# else?
