7272ed344c4af0b2e29ef9b28039377ced59cdb3
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-03-09 11:54:14 -0600

    Removed pending from 2nd test

--- bin/beer_on_the_wall.rb
+++ bin/beer_on_the_wall.rb
@@ -1,1 +1,6 @@
-puts "99 bottles of beer on the wall, 99 bottles of beer.\nTake one down and pass it around, 98 bottles of beer on the wall.\n"
+puts "How many bottles of beer?"
+bottles = Integer(gets.chomp)
+while bottles > 0
+  puts "#{bottles} bottles of beer on the wall, #{bottles} bottles of beer.\nTake one down and pass it around, #{bottles -= 1} bottles of beer on the wall.\n"
+end
+
