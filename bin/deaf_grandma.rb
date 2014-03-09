fbbcc35bfda799eb5da33c5ce2d37f6dccf4fce1
Kaylee Edmonson <kaylee.edmonson@gmail.comcore.autocrlf>
2014-03-09 10:23:46 -0600

    Deleted pending for second test

--- bin/deaf_grandma.rb
+++ bin/deaf_grandma.rb
@@ -1,4 +1,11 @@
 puts "Say hi to Grandma"
-  hi_grandma = gets.chomp ()
-puts "HUH?! SPEAK UP SONNY!"
+hi_grandma = gets.chomp ()
+
+
+if hi_grandma == hi_grandma.upcase
+  year = 1942
+  puts "NO, NOT SINCE #{year}!"
+else
+  puts "HUH?! SPEAK UP SONNY!"
+end
 
